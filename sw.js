63229f7599413105dcdbd704bee530a673640bce
angelElite <68104152+angelElite@users.noreply.github.com>
2022-11-30 23:51:12 -0600

    Update sw.js
    prueba 2

--- sw.js
+++ sw.js
@@ -6,13 +6,7 @@ workbox.precaching.precacheAndRoute([
   
     'hombre.html',
     'mujer.html',
-    'img',
-    //'app.js',
-    //'sw.js',
     'logica.js',
-    //'manifest.webmanifest',
-    'css/bootstrap.min.css',
-    'js/bootstrap.min.js',
     'offline.html'
 ])
 
@@ -40,13 +34,7 @@ workbox.routing.setCatchHandler(async context =>{
 });
 
 
-//self.addEventListener('install', (e)=>{
-    console.log("Instalado");
-    e.waitUntil(async()=>{
-      const cache = await caches.open(cacheName);
-      await cache.addAll(contenidoCache);
-    })
-//});
+
 
 
 
